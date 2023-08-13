83633e94938ba2c081b513634fe2a4e77b3c7ce9
Rishith S <rishithsaginala@gmail.com>
2023-08-13 19:59:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const people = [
   { user_id: '#1', name: 'Person 1', tickets: [] },
